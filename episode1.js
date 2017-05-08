28011c90c31ec409fbfa802e673d5045ea810a2f
Eden <edenstevens101@gmail.com>
2017-05-08 16:13:49 +0100

    Commented on first episode

--- episode1.js
+++ episode1.js
@@ -1,7 +1,7 @@
-var name = 'Keith';
+var name = 'Keith';   //Setting var to 'Keith'
 
 var printName = function() {
-  console.log('My name is ' + name );
+  console.log('My name is ' + name );  //prints out String plus Global Var name.
 };
 
-printName();
+printName(); //Will out put printName function with a name of 'Keith'
